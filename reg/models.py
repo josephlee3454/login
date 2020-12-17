2880d2a0c9937c829d6911c18569fd31bc0c421c
Joe <joseph.lee3454@gmail.com>
2020-12-16 17:41:59 -0800

    added the validator not finished though just have registration

--- reg/models.py
+++ reg/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class LoginManager(models.Model):
+    def reg_validator(self,postData):
+        errors = {}
+        email_filter = User.objects.filter(email = postData["email"])
+        if len(postData['first_name']) <2:
+            errors['firstnameerror'] = "First Name must be longer than two charcters"
+        if len(postData['last_name']) <2:
+            errors['lastnameerror'] = "Last Name must be longer than two charcters"
+        if len(postData['email_filter'])>0:
+            errors['emailused'] = "The email you entered is in use"
+        if postData['password'] != postData['repassword']:
+            errors['passnomatch'] = "your passwords do not match"
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    repassword = models.CharField(max_length=100)
+
+    def __str__(self):
+       return self.first_name + " " + self.last_name+ " "  + self.email + " " + self.password +" "+ self.repassword
+# First Name - required; at least 2 characters; letters only
+# Last Name - required; at least 2 characters; letters only
+# Email - required; valid format
+# Password - required; at least 8 characters; matches password confirmation
--- reg/views.py
+++ reg/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages 
+from .models import *
+
 
 # Create your views here.
+def index(request):
+   
+    return render(request, 'index.html')
+
+def sucess(request):
+   
+    return render(request, 'sucess.html')
+def regis(request):
+    if request.method == "POST":
+        errors = User.objects.reg_validator(request.POST)
+        if errors:
+            for key, value in errors.items():
+                messages.error(request, value) 
+            return redirect("/")
+        else:
+            user = User.objects.create(
+                first_name = request.POST['first_name'], 
+                last_name = request.POST['last_name'], 
+                email = request.POST['email'], 
+                password = request.POST['password'], 
+                repassword=request.POST['repassword'])
+            return redirect('/success')
+
